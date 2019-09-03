793cbe73131dea51c4f9167be404ea12f0c08c5b
liuyang <me@liuyangjob.cn>
2019-09-03 13:25:08 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-
+    static  class Cat{
+         public  static  String  name;
+     }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String s = Cat.name;
     }
 }
