1da5e68b89db1fdab9ab610d41b074872e47f530
刘洋 <250935102@QQ.COM>
2020-04-10 00:43:02 +0800

    刘洋
    急急急啊

--- src/cn/ly/leetcode/Mid/Mar31/Solution.java
+++ src/cn/ly/leetcode/Mid/Mar31/Solution.java
@@ -1,6 +1,5 @@
 package cn.ly.leetcode.Mid.Mar31;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,20 +8,20 @@ import java.util.List;
  */
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list =new ArrayList<>();
-        for(int i=0;i<numRows;i++){ //每行的个数
-            if(i==0){
-                List<Integer> templist =new ArrayList<Integer>();
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) { //每行的个数
+            if (i == 0) {
+                List<Integer> templist = new ArrayList<Integer>();
                 templist.add(1);
                 list.add(templist);
-            }else{
-                List<Integer> templist =new ArrayList<Integer>();
-                for(int j=0;j<i+1;j++){
-                    if(j==0||j==i){
+            } else {
+                List<Integer> templist = new ArrayList<Integer>();
+                for (int j = 0; j < i + 1; j++) {
+                    if (j == 0 || j == i) {
                         templist.add(1);
-                    }else{
-                        List<Integer>  shangyihang =list.get(i-1);
-                        templist.add(shangyihang.get(j-1)+shangyihang.get(j));
+                    } else {
+                        List<Integer> shangyihang = list.get(i - 1);
+                        templist.add(shangyihang.get(j - 1) + shangyihang.get(j));
                     }
 
                 }
@@ -32,22 +31,23 @@ class Solution {
 
         return list;
     }
+
     public List<List<Integer>> generate2(int numRows) {
-        List<List<Integer>> list =new ArrayList<>();
-        if(numRows==0){
+        List<List<Integer>> list = new ArrayList<>();
+        if (numRows == 0) {
             return list;
         }
-        if(numRows==1){
+        if (numRows == 1) {
             list.add(new ArrayList<Integer>());
             list.get(0).add(1);
             return list;
         }
-        list = generate2(numRows-1);
-        List<Integer> shangyihang = list.get(numRows-2);
-        List<Integer> temp =new ArrayList<Integer>();
+        list = generate2(numRows - 1);
+        List<Integer> shangyihang = list.get(numRows - 2);
+        List<Integer> temp = new ArrayList<Integer>();
         temp.add(1); //第一位是1
-        for(int i=1;i<numRows-1;i++){
-            temp.add(shangyihang.get(i-1)+shangyihang.get(i));
+        for (int i = 1; i < numRows - 1; i++) {
+            temp.add(shangyihang.get(i - 1) + shangyihang.get(i));
         }
         temp.add(1);//最后一位是1
         list.add(temp);
@@ -55,28 +55,27 @@ class Solution {
     }
 
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> list =new ArrayList<>();
-        if(rowIndex==0){
+        List<Integer> list = new ArrayList<>();
+        if (rowIndex == 0) {
             return list;
         }
-        if(rowIndex==1){
+        if (rowIndex == 1) {
 
             list.add(1);
             return list;
         }
-        list = getRow(rowIndex-1);
-        List<Integer> temp =new ArrayList<Integer>();
+        list = getRow(rowIndex - 1);
+        List<Integer> temp = new ArrayList<Integer>();
         temp.add(1); //第一位是1
-        for(int i=1;i<rowIndex-1;i++){
-            temp.add(list.get(i-1)+list.get(i));
+        for (int i = 1; i < rowIndex - 1; i++) {
+            temp.add(list.get(i - 1) + list.get(i));
         }
         temp.add(1);//最后一位是1
         return temp;
     }
+
     public static void main(String[] args) {
-        Solution s2=new Solution();
+        Solution s2 = new Solution();
         s2.generate2(5);
-=======
->>>>>>> 7f6a587e776f4fdbd129ce3d33e3224a3b13f00d
     }
 }
