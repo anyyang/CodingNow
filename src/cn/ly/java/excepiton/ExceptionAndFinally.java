85a6bddbcf534a1f122ecf08ee076b3271cc6ef2
刘洋 <liu.yang@autoyong.com>
2020-05-30 17:17:55 +0800

    异常抛出  和finnally之间优先级关系

--- src/cn/ly/java/excepiton/ExceptionAndFinally.java
+++ src/cn/ly/java/excepiton/ExceptionAndFinally.java
@@ -6,4 +6,23 @@ package cn.ly.java.excepiton;
  * email : liu.yang@autoyong.com
  **/
 public class ExceptionAndFinally {
+    public static void main(String[] args) {
+        ExceptionAndFinally    ea= new ExceptionAndFinally();
+        System.out.println(ea.throwMe());
+    }
+
+    public String throwMe(){
+        try{
+            throw new RuntimeException("000000");
+        }catch (Exception e){
+            System.out.println("111111111111");
+            e.printStackTrace();
+            throw e;
+        }
+        finally {
+           System.out.println("but i am fine");
+            return "hello this is test";
+        }
+    }
+
 }
