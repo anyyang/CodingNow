91ac926c4c7a6ad67e5f2f881b1dedb10a587fc1
刘洋 <250935102@QQ.COM>
2021-03-31 16:09:41 +0800

    liuyang
    feat： 内存逃逸分析

---
+++ src/main/java/cn/ly/EscapeAnalysisTest.java
@@ -0,0 +1,39 @@
+package cn.ly;
+
+/**
+ * jvm配置参数
+ * <p>
+ -Xmx4m -Xms4m
+  去除逃逸分析
+ -XX:-DoEscapeAnalysis
+ -XX:+PrintGCDetails
+ -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class EscapeAnalysisTest {
+    public static void main(String[] args) {
+        long a1 = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            alloc();
+        }
+
+        // 查看执行时间
+        long a2 = System.currentTimeMillis();
+        System.out.println("cost " + (a2 - a1) + " ms");
+        // 为了方便查看堆内存中对象个数，线程sleep
+        try {
+            Thread.sleep(100000);
+        } catch (
+                InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+    }
+
+    private static void alloc() {
+        User user = new User();
+    }
+
+
+    static class User {
+    }
+}
