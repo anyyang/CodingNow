b3dbeb6c2d78b39b32517826a0223aa7addf38dc
刘洋 <250935102@QQ.COM>
2021-08-23 15:08:36 +0800

    刘洋
    异步调度分布结果

--- src/main/java/cn/ly/manyThread/callback/CompleteableFutureTest.java
+++ src/main/java/cn/ly/manyThread/callback/CompleteableFutureTest.java
@@ -8,7 +8,7 @@ public class CompleteableFutureTest {
     public static void main(String[] args) {
         // 测试 异步使用是线程池
         //testCompletableFuture();
-       // 测试异步获取返回结果
+        // 测试异步获取返回结果
         testCompletionStage();
 
     }
@@ -30,29 +30,37 @@ public class CompleteableFutureTest {
         threadPoolExecutor.shutdown();
     }
 
+    /**
+     * 异步任务的调度编排
+     */
     public static void testCompletionStage() {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, new ArrayBlockingQueue(10));
 
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println("自己提供的线程池ThreadName:" + Thread.currentThread().getName());
             return 888;
-        }, threadPoolExecutor);
-        threadPoolExecutor.shutdown();
-        //线程池
-        CompletableFuture<Integer> futureNotPool = CompletableFuture.supplyAsync(() -> {
-            System.out.println("默认提供线程池ThreadName:" + Thread.currentThread().getName());
-            return 666;
-        });
+        }, threadPoolExecutor)
+                .thenApplyAsync((x) -> {
+                    x = 5 / 0;
+                    return x + 5;
+                }).whenComplete((x, e) -> {
+                    if (e != null) {
+                        e.printStackTrace();
+                    }
+                }).exceptionally(e -> {
+                    e.printStackTrace();
+                    return -100;
+                });
         try {
-            System.out.println("线程返回结果：" + future.get());
-            System.out.println("线程返回结果：" + futureNotPool.get());
+            Integer i = future.get();
+            System.out.println(i);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-
         threadPoolExecutor.shutdown();
+
     }
 
 }
